2f1299232545d9149ba44108c93996da918440a0
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2024-09-16 19:27:34 -0600

    Render the form data

--- script.js
+++ script.js
@@ -7,17 +7,45 @@ function formSubmission(){
     let birthdate = inputDate.value
     let email = inputEmail.value
     let comment = inputComment.value
+    let gender = inputGender.value
 
     let tmp = {
        name, 
        birthdate, 
        email, 
-       comment
+       comment, 
+       gender
     }
 
     data.push(tmp)
 
-    console.log(data)
+    renderData()
+
+}
+
+function renderData(){
+
+    let tableLayOut = `
+        <tr>
+            <th>Name</th>
+            <th>Birthdate</th>
+            <th>Email</th>
+            <th>Gender</th>
+        </tr>
+    `
+
+    for(let i=0; i < data.length; i++){
+        tableLayOut += `
+                        <tr> 
+                        <td> ${data[i].name} </td>
+                        <td> ${data[i].birthdate} </td>
+                        <td> ${data[i].email} </td>
+                        <td> ${data[i].gender} </td>
+                        </tr>`
+    }
+
+    dataOutput.innerHTML = tableLayOut
+
 
 }
 
